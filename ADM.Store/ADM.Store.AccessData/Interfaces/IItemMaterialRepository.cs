8899a3ff600e4ea2b90f7c07adfb519ca9f6e3a6
Luis Angel Martinez <luis.martinezh13gmail.com>
2022-07-13 09:05:04 -0500

    implementation for ItemMaterialRepository

---
+++ ADM.Store/ADM.Store.AccessData/Interfaces/IItemMaterialRepository.cs
@@ -0,0 +1,16 @@
+﻿using ADM.Store.Models.Models.ItemMaterial;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADM.Store.AccessData.Interfaces
+{
+    internal interface IItemMaterialRepository
+    {
+        Task<List<ItemMaterialDetailsModel>> ListByItemTypeAsync(int idItemType);
+        Task<bool> ExistsInItemTypeAsync(int idItemMaterial, int idItemType);
+        Task<bool> ExistsByNameInItemTypeAsync(string itemMaterialName, int idItemType);
+    }
+}
